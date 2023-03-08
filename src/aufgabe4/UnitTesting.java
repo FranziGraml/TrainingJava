71dc1e0c40268f5409c62c38ea36855bcf6cc57f
Franzi Graml <franzi.graml@gmx.de>
2023-03-08 20:59:06 +0100

    unitTesting

---
+++ src/aufgabe4/UnitTesting.java
@@ -0,0 +1,14 @@
+package aufgabe4;
+
+public class UnitTesting {
+
+    public static boolean isDivisibleByTwo(int number) {
+
+        if(number % 2 == 0)
+            return  true;
+
+        return false;
+    }
+
+
+}

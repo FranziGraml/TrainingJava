7c95beb5f58b9e64f4eb796acca2827db0b7537d
Franzi Graml <franzi.graml@gmx.de>
2023-03-08 20:29:23 +0100

    aufgabe1

---
+++ src/aufgabe1/Aufgabe1.java
@@ -0,0 +1,19 @@
+package aufgabe1;
+
+public class Aufgabe1 {
+
+
+    // a) 5  int 5
+// b) 5.0 double 5.0
+// c) 4L long 4
+// d) 3 + 9  int 12
+// e) 2 + 6.0    8.0 double
+// f) 5 + 3.0f    8.0 float
+// g) 3d + 1.0f   double 4
+// h) 6L * 3   long 18
+// i) 7 / 2  int 3
+// j) 9 / 2.0  double 4.5
+// k) int i = 2; i--; int 1
+
+}
+

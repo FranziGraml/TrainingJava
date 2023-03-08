774f4228531afd8db91164ae377452bd249b225c
Franzi Graml <franzi.graml@gmx.de>
2023-03-08 20:44:06 +0100

    aufgabe2.1

--- src/aufgabe2/Aufgabe2.java
+++ src/aufgabe2/Aufgabe2.java
@@ -1,62 +1,62 @@
-//package aufgabe2;
+package aufgabe2;
 
-//public class Aufgabe2 {
+public class Aufgabe2 {
 
 
-    //public static void main(String[] args) {
+    public static void main(String[] args) {
 
-//int i = 0;
-//int j = 7;
-//int k = 13;
-//
-//
-//// Snippet 1
-//if (i > 0 || j > 5) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 7, k= 10
-//
-//// Snippet 2
-//if (i > 0 && j > 5) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 7, k= 13
-//
-//// Snippet 3
-//if ((i > 0 && j > 5) || k < 15) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 7, k= 10
-//
-//// Snippet 4
-//if ((i > 0 || j > 5) && k > 15) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 7, k= 13
-//
-//// Snippet 5
-//if (i == 0 & j++ < 5) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 8, k= 13
-//
-//// Snippet 6
-//if (i == 0 && j++ < 5) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 8, k= 13
-//
-//// Snippet 7
-//if (i != 0 && j++ < 5) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 7, k= 13
-//
-//// Snippet 8
-//if (i != 0 & j++ < 5) {
-//    k = 10;
-//  }
-//// -> i= 0, j= 8, k= 13
-//
-    //}
-//}
+int i = 0;
+int j = 7;
+int k = 13;
+
+
+// Snippet 1
+if (i > 0 || j > 5) {
+    k = 10;
+  }
+// -> i= 0, j= 7, k= 10
+
+// Snippet 2
+if (i > 0 && j > 5) {
+    k = 10;
+  }
+// -> i= 0, j= 7, k= 13
+
+// Snippet 3
+if ((i > 0 && j > 5) || k < 15) {
+    k = 10;
+  }
+// -> i= 0, j= 7, k= 10
+
+// Snippet 4
+if ((i > 0 || j > 5) && k > 15) {
+    k = 10;
+  }
+// -> i= 0, j= 7, k= 13
+
+// Snippet 5
+if (i == 0 & j++ < 5) {
+    k = 10;
+  }
+// -> i= 0, j= 8, k= 13
+
+// Snippet 6
+if (i == 0 && j++ < 5) {
+    k = 10;
+  }
+// -> i= 0, j= 8, k= 13
+
+// Snippet 7
+if (i != 0 && j++ < 5) {
+    k = 10;
+  }
+// -> i= 0, j= 7, k= 13
+
+// Snippet 8
+if (i != 0 & j++ < 5) {
+    k = 10;
+  }
+// -> i= 0, j= 8, k= 13
+
+    }
+}

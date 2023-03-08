1c1c2b6da506af1c96afe668b087758a72ab0066
Franzi Graml <franzi.graml@gmx.de>
2023-03-08 20:49:32 +0100

    jumpStatements

---
+++ src/aufgabe3/ContinueStatement.java
@@ -0,0 +1,17 @@
+package aufgabe3;
+
+public class ContinueStatement {
+
+    public static void main (String[] args){
+
+
+
+        for(int i = 0; i < 10; i++){
+            if(i == 3){
+                continue;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
